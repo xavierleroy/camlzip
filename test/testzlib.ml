56e1a32208c9cb8397d970f9baabed8017948688
Ralf Treinen <treinen@free.fr>
2019-11-08 07:58:14 +0100

    fix testzlib.ml for ocaml.ml (String=>Bytes)

--- test/testzlib.ml
+++ test/testzlib.ml
@@ -1,7 +1,7 @@
 let compress infile outfile =
   let ic = open_in_bin infile
   and oc = open_out_bin outfile in
-  Zlib.compress (fun buf -> input ic buf 0 (String.length buf))
+  Zlib.compress (fun buf -> input ic buf 0 (Bytes.length buf))
                 (fun buf len -> output oc buf 0 len);
   close_in ic;
   close_out oc
@@ -9,7 +9,7 @@ let compress infile outfile =
 let uncompress infile outfile =
   let ic = open_in_bin infile
   and oc = open_out_bin outfile in
-  Zlib.uncompress (fun buf -> input ic buf 0 (String.length buf))
+  Zlib.uncompress (fun buf -> input ic buf 0 (Bytes.length buf))
                   (fun buf len -> output oc buf 0 len);
   close_in ic;
   close_out oc
